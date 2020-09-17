9fc81cc0f8ab886026823be3980e4839054d6f1c
Josh Mann <jlmann72@gmail.com>
2020-09-17 15:39:14 -0400

    Added props in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react'
+import { NewsArticle } from './components/NewsArticle'
 
 class App extends Component {
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <div className="all-main-content">
+        <main>
+          <NewsArticle
+            title="SDG Announces Hackathon!"
+            body="SDG announces the 2020 Summer Hackathon"
+          />
+          <NewsArticle
+            title="Student Graduation is Right Around The Corner"
+            body="Our next cohort of students will be graduating in just over a week"
+          />
+          <NewsArticle
+            title="SDG Standardizes on React"
+            body="React is the best library for learning front end Web"
+          />
+        </main>
+      </div>
+    )
   }
 }
 
---
+++ src/components/NewsArticle.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export class NewsArticle extends React.Component {
+  render() {
+    return (
+      <article className="intro-article">
+        <h2 className="article-title">{this.props.title}</h2>
+        <p>{this.props.body}</p>
+        <a className="read-more" href="#here">
+          read more about {this.props.title}
+        </a>
+      </article>
+    )
+  }
+}
